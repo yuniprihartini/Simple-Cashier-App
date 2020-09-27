025d7cbcd0a19e5c193c6ddb7e0e0d3292e9dce8
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-09-28 02:42:17 +0700

    Create product list

---
+++ src/data/Product.js
@@ -0,0 +1,22 @@
+export const product = [
+    {
+        label: "Book",
+        value: 5000
+    },
+    {
+        label: "Pencil",
+        value: 3000
+    },
+    {
+        label: "Rubber",
+        value: 2000
+    }
+    // {
+    //     name: "Book",
+    //     price: 5000
+    // },
+    // {
+    //     name: "Pencil",
+    //     price: 2000
+    // }
+]
