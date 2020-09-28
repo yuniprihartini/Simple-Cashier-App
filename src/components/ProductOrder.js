d4b1da5c9990b6ff2433db01ec75773bd2af630b
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-09-28 16:03:03 +0700

    Create ProductOrder contains Price and ProductSelected

---
+++ src/components/ProductOrder.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import '../assets/ProductOrder.css'
+import Price from './Price';
+import ProductSelected from './ProductSelected';
+
+class ProductOrder extends Component {
+    // state for selectedOption 
+    state = {
+        selectedOption: null
+    }
+
+    // handleChange to select every option
+    productHandleChange = selectedOption => {
+        this.setState({ selectedOption })
+    }
+    render() {
+        // this.state.selectedOption
+        const { selectedOption } = this.state
+        return (
+            <div className="Product-Order">
+                <ProductSelected
+                    selectedOption={selectedOption}
+                    productHandleChange={this.productHandleChange} />
+                <Price
+                    selectedOption={selectedOption}
+                    productHandleChange={this.productHandleChange} />
+            </div>
+        );
+    }
+}
+
+export default ProductOrder;
