c2604956ceb10de483ab30e7e67afa17aa0ed37f
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-09-28 20:34:28 +0700

    Add Quantity and function to add and reduce it

--- src/components/ProductOrder.js
+++ src/components/ProductOrder.js
@@ -3,20 +3,36 @@ import React, { Component } from 'react';
 import '../assets/ProductOrder.css'
 import Price from './Price';
 import ProductSelected from './ProductSelected';
+import Quantity from './Quantity';
 
 class ProductOrder extends Component {
     // state for selectedOption 
     state = {
-        selectedOption: null
+        selectedOption: null,
+        quantity: 0,
+        disable: true
     }
 
     // handleChange to select every option
     productHandleChange = selectedOption => {
         this.setState({ selectedOption })
     }
+
+    addProductQuantity = () => {
+        this.setState({ quantity: this.state.quantity + 1, disable: false })
+    }
+    reduceProductQuantity = () => {
+        let quantity = this.state.quantity
+        if (quantity <= 0) {
+            this.setState({ disable: true })
+        } else {
+            this.setState({ quantity: quantity - 1, disable: false })
+        }
+    }
+
     render() {
         // this.state.selectedOption
-        const { selectedOption } = this.state
+        const { selectedOption, quantity, disable } = this.state
         return (
             <div className="Product-Order">
                 <ProductSelected
@@ -24,6 +40,11 @@ class ProductOrder extends Component {
                     productHandleChange={this.productHandleChange} />
                 <Price
                     selectedOption={selectedOption} />
+                <Quantity
+                    quantity={quantity}
+                    addProductQuantity={this.addProductQuantity}
+                    reduceProductQuantity={this.reduceProductQuantity}
+                    disable={disable} />
             </div>
         );
     }
