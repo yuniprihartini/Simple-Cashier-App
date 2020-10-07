ffa277df48423b46a512743c6f9452c8e1339ce0
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-10-07 17:02:19 +0700

    Create add and remove button

--- src/components/ProductOrder.js
+++ src/components/ProductOrder.js
@@ -25,7 +25,7 @@ class ProductOrder extends Component {
     addProductQuantity = () => {
         this.setState({ quantity: this.state.quantity + 1, disable: false })
     }
-    
+
     // function to reduce quantity
     reduceProductQuantity = () => {
         let quantity = this.state.quantity
@@ -39,21 +39,25 @@ class ProductOrder extends Component {
     render() {
         const { selectedOption, quantity, total, disable } = this.state
         return (
-            <div className="Product-Order">
-                <ProductSelected
-                    selectedOption={selectedOption}
-                    productHandleChange={this.productHandleChange} />
-                <Price
-                    selectedOption={selectedOption} />
-                <Quantity
-                    quantity={quantity}
-                    addProductQuantity={this.addProductQuantity}
-                    reduceProductQuantity={this.reduceProductQuantity}
-                    disable={disable} />
-                <Total
-                    selectedOption={selectedOption}
-                    quantity={quantity}
-                    total={total} />
+            <div className="Product">
+                <button id="Add-Item" type="button" class="btn btn-primary" ><i class="fa fa-plus fa-sm" ></i> Add item</button>
+                <div className="Product-Order">
+                    <ProductSelected
+                        selectedOption={selectedOption}
+                        productHandleChange={this.productHandleChange} />
+                    <Price
+                        selectedOption={selectedOption} />
+                    <Quantity
+                        quantity={quantity}
+                        addProductQuantity={this.addProductQuantity}
+                        reduceProductQuantity={this.reduceProductQuantity}
+                        disable={disable} />
+                    <Total
+                        selectedOption={selectedOption}
+                        quantity={quantity}
+                        total={total} />
+                    <button id="Remove-Item" type="button" class="btn btn-danger"><i class="fa fa-trash fa-sm" ></i></button>
+                </div>
             </div>
         );
     }
